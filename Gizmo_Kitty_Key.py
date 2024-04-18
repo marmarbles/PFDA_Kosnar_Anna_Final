57b9c3f4a8668aa7483e4d4027346e99408e09f5
AnnaKosnar <annakosnar303@gmail.com>
2024-04-17 19:30:12 -0500

    Imported Code for Kitty Key Window

--- Gizmo_Kitty_Key.py
+++ Gizmo_Kitty_Key.py
@@ -9,7 +9,26 @@ pygame.init()
 pygame.display.set_caption("Gizmo's Kitty Key Adventure!")
 
 BG_COLOR = (255, 255, 255)
-WIDTH, HEIGHT = 100, 800
+WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5 
 
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+    clock = pygame.time.Clock()
+
+    run = True 
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main()
