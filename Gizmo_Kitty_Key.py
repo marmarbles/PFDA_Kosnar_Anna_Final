1f9179b9ed87e80563ef1264b85048819dd805ae
AnnaKosnar <annakosnar303@gmail.com>
2024-04-18 01:11:15 -0500

    fixed animation bugs, walk cycle compleate

--- Gizmo_Kitty_Key.py
+++ Gizmo_Kitty_Key.py
@@ -1,23 +1,23 @@
-import os 
-import random 
-import math 
-import pygame 
+import os
+import random
+import math
+import pygame
 from os import listdir
 from os.path import isfile, join
 pygame.init()
 
 pygame.display.set_caption("Gizmo's Kitty Key Adventure!")
 
-WIDTH, HEIGHT = 1000, 800
-FPS = 60 
-PLAYER_VEL = 5 
+WIDTH, HEIGHT = 700 , 500 
+FPS = 45 
+PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def flip(sprites):
     return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
 
-def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+def load_sprite_sheets(dir1, dir2,  frames, direction=False):
     path = join(dir1, dir2)  # Corrected path joining
     images = [f for f in listdir(path) if isfile(join(path, f))]
 
@@ -25,17 +25,19 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
 
     for image in images:
         sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        width = sprite_sheet.get_width() // frames
+        height = sprite_sheet.get_height()
 
         sprites = []
-        for i in range(sprite_sheet.get_width() // width): 
+        for i in range(sprite_sheet.get_width() // width):
             surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
             rect = pygame.Rect(i * width, 0, width, height)
             surface.blit(sprite_sheet, (0, 0), rect)
             sprites.append(pygame.transform.scale2x(surface))
-        
+
         if direction:
-            all_sprites["Idle_left"] = sprites  # Set the key as "Idle_left"
-            all_sprites["Idle_right"] = flip(sprites)  # Set the key as "Idle_right"
+            all_sprites[image.replace(".png", "_left")] = sprites  # Set the key as "Idle_left"
+            all_sprites[image.replace(".png", "_right")] = flip(sprites)  # Set the key as "Idle_right"
         else:
             all_sprites[image.replace(".png", "")] = sprites  # Set the key as the image filename without ".png"
 
@@ -45,20 +47,22 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
-    SPRITES = load_sprite_sheets("Sprites", "", 43, 35, True)
+    ANIMATION_DELAY = 5
 
     def __init__(self, x, y, width, height):
+        self.sprite = None
         self.rect = pygame.Rect(x, y, width, height)
-        self.x_vel = 0 
-        self.y_vel = 0 
-        self.mask = None 
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
         self.direction = "left"
-        self.animation_count = 0 
+        self.animation_count = 0
         self.fall_count = 0
+        self.SPRITES = load_sprite_sheets("Sprites", "", 6, True)
 
     def move(self, dx, dy):
-        self.rect.x += dx 
-        self.rect.y += dy 
+        self.rect.x += dx
+        self.rect.y += dy
 
     def move_left(self, vel):
         self.x_vel = -vel
@@ -67,19 +71,31 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def move_right(self, vel):
-        self.x_vel = vel 
+        self.x_vel = vel
         if self.direction != "right":
             self.direction = "right"
             self.animation_count = 0
+
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet.capitalize() + "_" + self.direction  # Capitalize sprite_sheet
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
 
     def loop(self, fps):
         # self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
+        self.update_sprite()
 
     def draw(self, win):
-        self.sprite = self.SPRITES["Idle_" + self.direction][0]  # Access the sprite using "Idle_" + direction
         win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
@@ -93,21 +109,21 @@ def get_Background(name):
             pos = (i * width, j * height)
             tiles.append(pos)
 
-    return tiles, image 
+    return tiles, image
 
 def draw(window, background, bg_image, player):
     for tile in background:
         window.blit(bg_image, tile)
 
     player.draw(window)
-        
+
     pygame.display.update()
 
 
 def handle_move(player):
     keys = pygame.key.get_pressed()
 
-    player.x_vel = 0 
+    player.x_vel = 0
     if keys[pygame.K_a]:
         player.move_left(PLAYER_VEL)
     if keys[pygame.K_d]:
@@ -118,12 +134,12 @@ def main():
     clock = pygame.time.Clock()
     background, bg_image = get_Background("PFDA_Background.png")
 
-    player = Player(100, 100, 50, 50)
+    player = Player(100, 100, 40, 31)
 
     pygame.mixer.music.load("Pawprint_Panic!.mp3")
     pygame.mixer.music.play(-1)  # -1 means loop indefinitely
 
-    run = True 
+    run = True
     while run:
         clock.tick(FPS)
 
@@ -131,7 +147,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        
+
         player.loop(FPS)
         handle_move(player)
         draw(window, background, bg_image, player)
